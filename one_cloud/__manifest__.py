7c82a486405312fa74bbd3946bf61587f0e5ebf5
quick-sort@outlook.com <quick-sort@outlook.com>
2026-08-23 21:44:43 +0800

    add web favicon

--- one_cloud/__manifest__.py
+++ one_cloud/__manifest__.py
@@ -2,11 +2,11 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
 {
-    "name": "云平台",
+    "name": "One Cloud",
     "summary": "云账号与凭证管理基座，配合 component 多态扩展各云服务商",
     "version": "19.0.1.0.0",
     "category": "Technical",
-    "author": "Rui",
+    "author": "quick-sort",
     "license": "LGPL-3",
     "installable": True,
     "application": True,
